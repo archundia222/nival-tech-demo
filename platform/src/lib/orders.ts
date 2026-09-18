fcc18f25830b9cb5341e392451c1792b54831ee7
archundia222 <rodrigo.ar.company@gmail.com>
2026-09-17 20:57:30 -0600

    Add Nival Pay extra section product

--- platform/src/lib/orders.ts
+++ platform/src/lib/orders.ts
@@ -5,3 +5,6 @@ export function money(amountCents: number) {
   return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(amountCents / 100);
 }
 
+
+export const NIVAL_PAY_EXTRA_SECTION_PRICE_CENTS = 1000;
+export const NIVAL_PAY_EXTRA_SECTION_PRODUCT = 'nival_pay_extra_section';
